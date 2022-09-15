c6a3db4dfd749da7ca361a1edffd38d95eec1d4e
Thiago <devtsarcangelo@gmail.com>
2022-09-15 00:24:34 -0300

    Robo de cotação de moedas

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { launch } from 'puppeteer';
+
+
+async function Robot() {
+    // const browser = await puppeteer.launch({headless: false}); // lançando o navegador - headless => propriedade que se true oculta o navegador
+    const browser = await launch(); // lançando o navegador
+    const page = await browser.newPage(); // abrindo uma aba nova
+    const moedaBase = `yuan`;
+    const moedaFinal = 'real';
+    const searchUrl = `https://www.google.com/search?q=${moedaBase}+para+${moedaFinal}&client=firefox-b-lm&sxsrf=ALiCzsaN5Hyx0ycP6_XKBbZSFfJYJSH2WA%3A1663211000042&ei=-JUiY--eAvGn5OUPjsmayAk&ved=0ahUKEwjv-t_m55X6AhXxE7kGHY6kBpkQ4dUDCA0&uact=5&oq=dolar+para+real&gs_lcp=Cgdnd3Mtd2l6EAMyCQgjECcQRhCCAjIFCAAQgAQyCAgAEIAEEIsDMggIABCABBCLAzIICAAQgAQQiwMyCAgAEIAEEIsDMggIABCABBCLAzIICAAQgAQQiwMyCAgAEIAEEIsDMggIABCABBCLAzoKCAAQRxDWBBCwAzoHCAAQsAMQQzoECCMQJzoHCAAQQxCLAzoECAAQQzoECC4QQzoKCC4Q1AIQQxCLAzoHCC4Q1AIQQzoSCCMQJxCLAxCmAxCoAxBGEIICSgQIQRgASgQIRhgAULcHWOolYM4paANwAXgAgAGtAYgBxw-SAQQwLjE3mAEAoAEByAEKuAEDwAEB&sclient=gws-wiz`
+    await page.goto(searchUrl); // preenchendo um endereço
+    // await page.screenshot({path: 'example.png'}); // salvando um print do site
+
+    const resultado = await page.evaluate(() => {  // pega valores do devtools do navegador em sintaxe de frontend
+      return  document.querySelector('.lWzCpb.a61j6').value;
+    });
+
+     
+    console.log(`O valor de 1 ${moedaBase} em ${moedaFinal} é ${resultado}`)
+    // await browser.close(); // fechando navegador
+}
+
+Robot();
+
